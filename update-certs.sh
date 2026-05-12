29b86d31db0863ed8d92d146f519cef74a54aa5e
Dmitry.Normanev <Dmitry.Normanev@x5.ru>
2026-05-12 08:57:33 +0300

    feat: поддержка двух хостов

--- update-certs.sh
+++ update-certs.sh
@@ -20,3 +20,22 @@ else
     echo "Сертификаты для $SITE_HOST уже существуют."
     certbot renew --webroot -w $WEBROOT_PATH --non-interactive --deploy-hook "nginx -s reload"
 fi
+
+
+CERT_DIR="/etc/letsencrypt/live/$SITE_HOST_SECOND"
+CERT_PATH="$CERT_DIR/fullchain.pem"
+KEY_PATH="$CERT_DIR/privkey.pem"
+
+if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
+    echo "Сертификаты для $SITE_HOST_SECOND не найдены или неполны. Получаем новые..."
+    certbot certonly --webroot \
+        -w $WEBROOT_PATH \
+        -d $SITE_HOST_SECOND \
+        --email $EMAIL \
+        --agree-tos \
+        --non-interactive \
+        --deploy-hook "nginx -s reload"
+else
+    echo "Сертификаты для $SITE_HOST_SECOND уже существуют."
+    certbot renew --webroot -w $WEBROOT_PATH --non-interactive --deploy-hook "nginx -s reload"
+fi
